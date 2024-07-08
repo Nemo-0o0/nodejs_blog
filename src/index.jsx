a264993fa20bdd6849470e286629be3f106e7b52
das-110722 <phuocvu.hcmc@gmail.com>
2024-07-09 01:37:41 +0700

    Template engine (handlebars)

---
+++ src/index.jsx
@@ -0,0 +1,29 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const app = express()
+const port = 1203
+
+
+// HTTP Logger
+app.use(morgan('combined'))
+
+// Template Engine
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
