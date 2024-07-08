08f3bfce1abe449d25d6583bf9f40f97a28427fe
das-110722 <phuocvu.hcmc@gmail.com>
2024-07-08 09:58:08 +0700

    Install Morgan

--- index.jsx
+++ index.jsx
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 const port = 1203
 
+app.use(morgan('combined'))
 app.get('/', (req, res) => {
   return res.send('Hello World!')
 })
