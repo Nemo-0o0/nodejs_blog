eb74bfa6cf5c3dff3e51ff939392a3aeb91b4b95
das-110722 <phuocvu.hcmc@gmail.com>
2024-07-08 09:45:11 +0700

    nodejs-blog

---
+++ index.jsx
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 1203
+
+app.get('/', (req, res) => {
+  return res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
