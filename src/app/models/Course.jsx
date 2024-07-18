970a474cb49abfe830cf44e6b49bcfc21e6baca2
das-110722 <phuocvu.hcmc@gmail.com>
2024-07-18 16:01:39 +0700

    [CRUD] Delete course

--- src/app/models/Course.jsx
+++ src/app/models/Course.jsx
@@ -6,17 +6,17 @@ mongoose.plugin(slug);
 const Schema = mongoose.Schema;
 
 const Course = new Schema(
-    {
-        name: { type: String, required: true },
-        description: { type: String },
-        image: { type: String },
-        videoId: { type: String, required: true },
-        level: { type: String },
-        slug: { type: String, slug: 'name' },
-    },
-    {
-        timestamps: true,
-    },
+      {
+            name: { type: String, required: true },
+            description: { type: String },
+            image: { type: String },
+            videoId: { type: String, required: true },
+            level: { type: String },
+            slug: { type: String, slug: 'name' },
+      },
+      {
+            timestamps: true,
+      }
 );
 
 module.exports = mongoose.model('Course', Course);
--- src/config/db/index.jsx
+++ src/config/db/index.jsx
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 function connect() {
-    mongoose
-        .connect('mongodb://127.0.0.1:27017/f8_education_dev')
-        .then(() => console.log('MongoDB Connected...'))
-        .catch(() => console.log(' Error '));
+      mongoose
+            .connect('mongodb://127.0.0.1:27017/f8_education_dev')
+            .then(() => console.log('MongoDB Connected...'))
+            .catch(() => console.log(' Error '));
 }
 
 module.exports = { connect };
